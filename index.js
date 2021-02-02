34634013fd1906401c2a0d28000a24842f003f9e
Jason Fleshel <jfleshel@temple.edu>
2021-02-02 16:34:01 -0500

    switched to port 80

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ app.get('/', (resquest, response, next) => {
 	res.send('test');
 });
 
-const server = http.createServer(app).listen(8888, () => {
-	console.log('server lisetening on port 8888');
+const server = http.createServer(app).listen(80, () => {
+	console.log('server lisetening on port 80');
 });

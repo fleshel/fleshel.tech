c6914396873808e98ebe942af3ac6939a742d331
Jason Fleshel <jfleshel@temple.edu>
2021-02-02 16:11:58 -0500

    index test

---
+++   index.js
@@ -0,0 +1,15 @@
+const http = require('http')
+	,  fs = require('fs')
+
+const express = require('express')
+const app = express();
+
+const _PORT_ = 8888;
+
+app.get('/', (resquest, response, next) => {
+	res.send('test');
+});
+
+const server = http.createServer(app).listen(8888, () => {
+	console.log('server lisetening on port 8888');
+});
